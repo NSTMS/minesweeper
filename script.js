3823aeb2dd91582c124fc180c98d28b848d9d049
nstms <jan.idzi1@gmail.com>
2022-09-14 20:26:01 +0200

    nesesery fixes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let minesArr = new Set()
 let cubesArray,helper, wasChecked = new Array()
-let interval,firstClick = 0
-
+let interval,firstClick,s_width,s_height,mines,nick = 0
 class Cube{
     constructor(){
         this.id = 0
@@ -19,20 +18,35 @@ class Cube{
     }
 }
 
+function validation()
+{
+    s_width = parseInt(document.getElementById("form_width").value)
+    s_height = parseInt(document.getElementById("form_height").value)
+    mines = parseInt(document.getElementById("form_mines").value)
+    nick = document.getElementById("form_nick").value
+
+    if(nick.length == 0 || s_width <= 0 || s_height <=0 || mines <= 2)
+    {
+        interval = clearInterval(interval)
+        alert("mordo, złe dane")
+    }else generateSweeper()
+
+}
+
+
 function generateSweeper(){
 
-    const s_width = document.getElementById("form_width").value
-    const s_height = document.getElementById("form_height").value
-    const mines = document.getElementById("form_mines").value
     const temp = s_width*s_height;
 
     // generate grid with exact same width and height as cubes
     document.getElementById("main_nav").style.width = `${s_width*50}px`
     document.getElementById("main_content").style.width = `${s_width*50}px`
     document.getElementById("main_content").style.height = `${s_height*50}px`
+
     document.getElementById("main_content").style.gridTemplateColumns = `repeat(${s_width}, 50px)`
     document.getElementById("main_content").style.gridTemplateRows = `repeat(${s_height},50px)`
-
+    
+    // document.getElementById("leaderboard").style.height = document.getElementById("main_content").style.height 
     // set timer
     interval = clearInterval(interval)
     let minutes = 0
@@ -51,9 +65,11 @@ function generateSweeper(){
     },100)
 
     // settings reset
-    cubesArray, helper = []
-    wasChecked.length = 0
+    document.getElementById("main_content").style.pointerEvents = "all"
+    helper = []
+    wasChecked = []
     firstClick = 0
+    cubesArray = []
     document.getElementById("main_content").innerHTML = ``
     for(let i=0;i<temp;i++) wasChecked.push(i)
 
@@ -74,53 +90,51 @@ function generateMines(mines, max)
 
 function createCubes()
 {
-    const s_width = document.getElementById("form_width").value
-    const s_height = document.getElementById("form_height").value
     let counter = 0
-    cubesArray = new Array(s_height)
     
     // fill cubes tab
-    for(let i=0;i<s_height;i++){
+    cubesArray = new Array(s_height)
+    for(let i=0;i<s_height;i++)
+    {
         cubesArray[i] = new Array(s_width)
-        for(let j=0;j<s_width;j++){
+        for(let j=0;j<s_width;j++)
+        {
             const square = new Cube()
-            square.x = i;
-            square.y = j;
+            square.x = i
+            square.y = j
             square.id = counter
-            if(minesArr.has(counter))
-            {
-                square.isMine = true
-            }
+            if(minesArr.has(counter)) square.isMine = true
             cubesArray[i][j] = square
             helper.push(square)
             square.generateCube()
+            document.getElementById(counter).style.pointerEvents = "all"
             counter++
         }
+
     }
 
-    // how many mines around
     counter = 0
-    for(let y=0;y<s_height;y++)
+    for(let x=0;x<s_height;x++)
     {
-        for(let x=0;x<s_width;x++)
+        for(let y=0;y<s_width;y++)
         {
             if(x != 0)
             {
                 if(cubesArray[x-1][y].isMine == true) counter+=1
                 if(y != 0) if(cubesArray[x-1][y-1].isMine == true) counter+=1
-                if(y != s_height-1) if(cubesArray[x-1][y+1].isMine == true) counter+=1
+                if(y != s_width-1) if(cubesArray[x-1][y+1].isMine == true) counter+=1
             }
-
+            
             if(y != 0) if(cubesArray[x][y-1].isMine == true) counter+=1
             
-            if(x != s_width-1)
+            if(x !== s_height-1)
             {
                 if(cubesArray[x+1][y].isMine == true) counter+=1
                 if(y != 0) if(cubesArray[x+1][y-1].isMine == true) counter+=1
-                if(y != s_height-1) if(cubesArray[x+1][y+1].isMine == true) counter+=1
+                if(y != s_width-1) if(cubesArray[x+1][y+1].isMine == true) counter+=1
             }
-
-            if(y != s_height-1) if(cubesArray[x][y+1].isMine == true) counter+=1
+            
+            if(y != s_width-1) if(cubesArray[x][y+1].isMine == true) counter+=1
             
             cubesArray[x][y].minesAround = counter
             counter = 0
@@ -130,89 +144,112 @@ function createCubes()
 
 
 document.getElementById("main_content").addEventListener("click" ,(event)=>{
-    cubesReveal(helper[event.target.id])
+    if(event.target.id != "main_content") cubesReveal(helper[event.target.id])
 })
 
 function cubesReveal(cube)
 {   
     areYouWinningSon()
-    const s_width = document.getElementById("form_width").value
-    const s_height = document.getElementById("form_height").value
-    if(cube.x != 'undefinded' || cube.y != 'undefinded')
+    if(cube != 'undefinded')
     {
         let x = parseInt(cube.x)
         let y = parseInt(cube.y)
         let checked = (wasChecked.includes(cube.id) == true)? true : false
 
-        if(cube.isMine != true)
+        if(cube.isMine ==  false)
         {
             if(cube.minesAround == 0 && checked == true)
             {
-             
                 wasChecked.splice(wasChecked.indexOf(cube.id),1)
-                areYouWinningSon()
-             
-                // console.log(`--------${cube.id}------------`)
-                temporary(x,y,"-1+","-1+",cube)
-                temporary(x,y,"-","-",cube)
-                temporary(x,y,"-1+","-",cube)
-                temporary(x,y,"+","-",cube)
-                temporary(x,y,"-","-1+",cube)
-                temporary(x,y,"+","-1+",cube)
-                temporary(x,y,"-","+",cube)
-                temporary(x,y,"-1+","+",cube)
-                temporary(x,y,"+","+",cube)
+                
+                if(x != 0)
+                {
+                    if(cubesArray[x-1][y] != 'undefinded') fill((x-1),(y))
+                    if(y != 0) if(cubesArray[x-1][y-1] != 'undefinded') fill((x-1),(y-1))
+                    if(y != s_width-1) if(cubesArray[x-1][y+1] != 'undefinded') fill((x-1),(y+1))
+                }
+    
+                if(y != 0) if(cubesArray[x][y-1] != 'undefinded') fill(x,(y-1))
+                
+                if(x != s_height-1)
+                {
+                    if(cubesArray[x+1][y] != 'undefinded') fill((x+1),y)
+                    if(y != 0) if(cubesArray[x+1][y-1] != 'undefinded') fill((x+1),(y-1))
+                    if((y != s_width-1) && (x != s_height-1)) if(cubesArray[x+1][y+1] != 'undefinded') fill((x+1),(y+1))
+                }
+
+                if(y != s_width-1) if(cubesArray[x][y+1] != 'undefinded') fill((x),(y+1))
+
             }
-            else if(cube.minesAround != 0 && checked == true){
+            else if(checked == true && cube.minesAround != 0){
+                document.getElementById(cube.id).style.pointerEvents = "none"
                 document.getElementById(cube.id).style.background = "blue"
                 document.getElementById(cube.id).innerText = cube.minesAround
-                document.getElementById(cube.id).style.pointerEvents = "none"
 
                 wasChecked.splice(wasChecked.indexOf(cube.id),1)
-                areYouWinningSon()
             }
+            areYouWinningSon()
         }
-        else{
-            alert("PRZEGEAŁEŚ SYNU"); generateSweeper()
-        }
+        else endGame()
     }
 
-
-} 
-        
-function temporary(x,y,char1,char2,cube)
-{   
-    x = eval(`${x}${char1}1`)
-    y = eval(`${y}${char2}1`)
-    try{  
-        areYouWinningSon()
-        if(typeof(cubesArray[x][y]) !== 'undefined')
-            if(cubesArray[x][y].isMine != true)
-            {
-                    if(cubesArray[x][y].minesAround == 0 && cubesArray[x][y].isMine != true) document.getElementById(cubesArray[x][y].id).style.background = "yellow"
-                    else document.getElementById(cubesArray[x][y].id).style.background = "blue"
-             }
-            else document.getElementById(cubesArray[x][y].id).style.background = "red"
-
-            document.getElementById(cubesArray[x][y].id).style.pointerEvents = "none"
-            cubesReveal(cubesArray[x][y])
-    }catch(e){}
 }
 
 
+function fill(x,y)
+{
+        if(cubesArray[x][y].minesAround == 0) 
+        {
+            document.getElementById(cubesArray[x][y].id).style.background = "yellow"
+
+        }
+        if(cubesArray[x][y].minesAround !=0){
+            document.getElementById(cubesArray[x][y].id).style.pointerEvents = "none"
+            document.getElementById(cubesArray[x][y].id).style.background = "blue"
+            document.getElementById(cubesArray[x][y].id).innerText = cubesArray[x][y].minesAround
+
+        }
+        cubesReveal(cubesArray[x][y])
+}
+        
 function areYouWinningSon()
 {
-    if(wasChecked.length == 10) if(confirm("WYGRAŁEŚ SYNU") == true) generateSweeper()
+    if(wasChecked.length == 10) 
+    {
+        // if(confirm("WYGRAŁEŚ SYNU") == true) generateSweeper()
+        // else 
+        document.getElementById("main_content").style.pointerEvents = "none"
+        interval = clearInterval(interval)
+
+        minesArr.forEach(element => {
+            document.getElementById(element).style.background = "red"
+        });
+        alert("siema wygrałeś")
+    }
 
 }
+function endGame()
+{
+    interval = clearInterval(interval)
+    alert("siema przegrałeś")
 
+    minesArr.forEach(element => {
+        document.getElementById(element).style.background = "red"
+    });
+
+
+}
 
 
 document.getElementById("main_content").addEventListener("mouseup" ,(event)=>{
-    if(event.button == 2) 
-    {
-        document.getElementById(event.target.id)
-
-    }
-
+//event.path[0]
+        if((event.which == 3) || (event.path[0].id != "main_content"))
+        {
+            
+            if(wasChecked.includes([event.path[0].id]))
+            {
+                event.path[0].classList.toggle("flag")
+        
+            }
+        }
 });
